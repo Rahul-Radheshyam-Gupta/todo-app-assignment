40025094dacd0d03792e37bd231d63e776df0db2
Rahul Gupta <developer3@teamnest.com>
2022-11-21 10:17:26 +0530

    updated setting for production use

--- todo/settings.py
+++ todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8)v#ruo1xc$9_ntx%s5ias538so7dnv9q42926)!ys$4bc7rrn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://todo-app-by-rk.herokuapp.com/']
 
 
 # Application definition
